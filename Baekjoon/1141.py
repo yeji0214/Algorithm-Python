261cace8f17f77fbc781962579d09ea301944a47
yeji0214 <1225yj50@naver.com>
2024-03-02 16:29:59 +0900

    백준 1141

---
+++ Baekjoon/1141.py
@@ -0,0 +1,42 @@
+# 그리디
+# 내 풀이
+N = int(input())
+words = []
+ans = 0
+
+for _ in range(N):
+    words.append(input())
+
+for i in range(N):
+    for j in range(N):
+        if i == j:
+            continue
+        if words[j].find(words[i]) == 0: # 0일 때 접두사
+            words[i] = ""
+        
+for j in words:
+    if j != "":
+        ans += 1
+
+print(ans)
+
+# 정렬 사용
+N = int(input())
+words = []
+ans = 0
+
+for _ in range(N):
+    words.append(input())
+
+words.sort(key=len)
+
+for i in range(N):
+    for j in range(i+1, N):
+        if words[j].find(words[i]) == 0: # 0일 때 접두사
+            words[i] = ""
+        
+for j in words:
+    if j != "":
+        ans += 1
+
+print(ans)
