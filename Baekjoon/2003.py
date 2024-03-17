b3c14973f0b0535473c552e1ac3c8c47f35bdb81
yeji0214 <1225yj50@naver.com>
2024-03-17 21:06:47 +0900

    백준 2003

---
+++ Baekjoon/2003.py
@@ -0,0 +1,21 @@
+# 브루트포스, 투포인터
+N, M = map(int, input().split())
+num = list(map(int, input().split()))
+s = e = 0
+ans = 0
+
+while s <= e < N:
+    if M == sum(num[s:e+1]):
+        ans += 1
+        s += 1
+        e += 1
+    elif M > sum(num[s:e+1]):
+        e += 1
+    else:
+        if s == e:
+            s += 1
+            e += 1
+        else:
+            s += 1
+
+print(ans)
