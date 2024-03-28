44043a169ccbcb4ca439eaa272eeb8b8df4ffe87
yeji0214 <1225yj50@naver.com>
2024-03-28 20:31:19 +0900

    백준 9017

---
+++ Baekjoon/9017.py
@@ -0,0 +1,25 @@
+# 구현
+T = int(input())
+
+for _ in range(T):
+    N = int(input())
+    t = list(map(int, input().split()))
+    out = [] # 참가 선수가 6명 미만인 팀 번호 저장
+    score = {}
+    num = {} # 각 팀별 몇 번째 선수까지 검사했는지 저장
+
+    # 6명 미만인 팀 번호 저장
+    for i in t:
+        if t.count(i) < 6:
+            out.append(i)
+        else:
+            # 점수와 횟수 초기화
+            score[i] = []
+    
+    current = 1
+    for i in t:
+        if i not in out:
+            score[i].append(current)
+            current += 1
+
+    print(sorted(score, key= lambda x: (sum(score[x][0:4]), score[x][4]))[0])
