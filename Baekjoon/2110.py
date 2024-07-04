d9afbbd8c1c366699b9eb7260bc7cbd09f7136f3
yeji0214 <1225yj50@naver.com>
2024-07-05 02:33:24 +0900

    백준 - 2110 재풀이

--- Baekjoon/2110.py
+++ Baekjoon/2110.py
@@ -5,18 +5,18 @@ ans = 0
 
 while s <= e:
     mid = (s + e) // 2
-    total = 1 # 설치 가능한 공유기의 개수
+    total = 1
     current = home[0]
 
     for h in range(1, len(home)):
-        if home[h] >= current + mid:
+        if home[h] - current >= mid:
             total += 1
             current = home[h]
 
-    if total >= C:
+    if total < C:
+        e = mid - 1
+    else:
         s = mid + 1
         ans = mid
-    else:
-        e = mid - 1
 
 print(ans)
