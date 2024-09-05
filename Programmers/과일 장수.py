90d82c17b9a33d49ea32efa1ad18d2cb2b44347c
yeji0214 <1225yj50@naver.com>
2024-09-05 20:15:36 +0900

    프로그래머스 - 과일 장수

---
+++ Programmers/과일 장수.py
@@ -0,0 +1,12 @@
+def solution(k, m, score):
+    answer = 0
+    s = 0
+    score = sorted(score, reverse=True)
+    
+    while s + m <= len(score):
+        answer += min(score[s:s+m]) * m
+        s += m
+        
+    return answer
+
+print(solution(4, 3, [4, 1, 2, 2, 4, 4, 4, 4, 1, 2, 4, 2]))
