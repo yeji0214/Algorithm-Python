c24c498a3740a2c9e0860a2f554913b79ba74980
yeji0214 <1225yj50@naver.com>
2024-10-08 20:11:36 +0900

    백준 10819

--- Baekjoon/10819.py
+++ Baekjoon/10819.py
@@ -1,17 +1,15 @@
 from itertools import permutations
 
-n = int(input())
-arr = list(map(int ,input().split()))
+N = int(input())
+A = list(map(int, input().split()))
+ans = 0
 
-p = list(permutations(arr, n))
-def calculator(li):
-  total = 0
-  for i in range(len(li)-1):
-    total += abs(li[i]-li[i+1])
-  return total
+a = list(map(list,permutations(A, N)))
 
-answer = -1e9
-for li in p:
-  answer = max(answer, calculator(li))
+for i in range(len(a)):
+    res = 0
+    for j in range(N - 1):
+        res += abs(a[i][j] - a[i][j + 1])
+    ans = max(ans, res)
 
-print(answer)
+print(ans)
