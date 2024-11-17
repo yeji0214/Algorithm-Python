0748942857cde1b048bae5e3e32bc3509138c8b5
yeji0214 <1225yj50@naver.com>
2024-11-17 19:52:16 +0900

    백준 14888 재풀이

--- Baekjoon/14888.py
+++ Baekjoon/14888.py
@@ -1,32 +1,27 @@
-# 백트래킹
+def dfs(n, sm, add, sub, mul, div):
+    global maximum, minimum
+
+    if n == N:
+        maximum = max(maximum, sm)
+        minimum = min(minimum, sm)
+        return
+    
+    if add > 0:
+        dfs(n + 1, sm + A[n], add - 1, sub, mul, div)
+    if sub > 0:
+        dfs(n + 1, sm - A[n], add, sub - 1, mul, div)
+    if mul > 0:
+        dfs(n + 1, sm * A[n], add, sub, mul - 1, div)
+    if div > 0:
+        dfs(n + 1, int(sm / A[n]), add, sub, mul, div - 1)
+
 N = int(input())
 A = list(map(int, input().split()))
-O = list(map(int, input().split()))
+add, sub, mul, div = map(int, input().split())
 
-# 여기 1e9로 했을 때는 오류 났음 1e9 = 10억
 maximum = -float('inf')
 minimum = float('inf')
 
-def dfs(depth, total, plus, minus, multiply, divide):
-    global maximum, minimum
-
-    if depth == N:
-        maximum = max(maximum, total)
-        minimum = min(minimum, total)
-        return
-    
-    if plus:
-        dfs(depth + 1, total + A[depth], plus - 1, minus, multiply, divide)
-    if minus:
-        dfs(depth + 1, total - A[depth], plus, minus - 1, multiply, divide)
-    if multiply:
-        dfs(depth + 1, total * A[depth], plus, minus, multiply - 1, divide)
-    if divide:
-        if total < 0:
-            dfs(depth + 1, -(abs(total) // A[depth]), plus, minus, multiply, divide - 1)
-        else:
-            dfs(depth + 1, total // A[depth], plus, minus, multiply, divide - 1)
-
-dfs(1, A[0], O[0], O[1], O[2], O[3])
+dfs(1, A[0], add, sub, mul, div)
 print(maximum)
 print(minimum)
