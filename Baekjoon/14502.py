aac1d827583b6d88d6d58554e5314972e4ab3d78
yeji0214 <1225yj50@naver.com>
2024-11-29 16:23:47 +0900

    백준 14502

--- Baekjoon/14502.py
+++ Baekjoon/14502.py
@@ -56,62 +56,121 @@
 
 # print(ans)
 
-from itertools import combinations
-from collections import deque
-import copy
+# -----
 
-def bfs():
-    q = deque()
+# from itertools import combinations
+# from collections import deque
+# import copy
+
+# def bfs():
+#     q = deque()
     
-    for v in virus:
-        q.append((v[0], v[1]))
+#     for v in virus:
+#         q.append((v[0], v[1]))
+
+#     while q:
+#         cx, cy = q.popleft()
+
+#         for i in range(4):
+#             nx, ny = cx + dx[i], cy + dy[i]
+
+#             if 0 <= nx < N and 0 <= ny < M and temp[nx][ny] == 0:
+#                 temp[nx][ny] = 2
+#                 q.append((nx, ny))
+
+#     cnt = 0
+
+#     for i in range(N):
+#         cnt += temp[i].count(0)
+
+#     return cnt
+
+
+# N, M = map(int, input().split())
+# lab = [[0] *  M for _ in range(N)]
+
+# wall = []
+# virus = []
+# ans = 0
+
+# dx = [-1, 1, 0, 0]
+# dy = [0, 0, -1, 1]
+
+# for i in range(N):
+#     lab[i] = list(map(int, input().split()))
+
+# for i in range(N):
+#     for j in range(M):
+#         if lab[i][j] == 0:
+#             wall.append([i, j])
+#         elif lab[i][j] == 2:
+#             virus.append([i, j])
+
+# wall_list = list(combinations(wall, 3))
+
+# for w in wall_list:
+#     temp = copy.deepcopy(lab)
+#     temp[w[0][0]][w[0][1]] = 1
+#     temp[w[1][0]][w[1][1]] = 1
+#     temp[w[2][0]][w[2][1]] = 1
+
+#     ans = max(ans, bfs())
+
+# print(ans)
+
+import copy
+from collections import deque
+
+def spread_virus(temp):
+    global safe
+
+    q = deque()
+
+    for x, y in virus:
+        q.append((x, y))
 
     while q:
         cx, cy = q.popleft()
 
-        for i in range(4):
-            nx, ny = cx + dx[i], cy + dy[i]
+        for dx, dy in ((0, 1), (0, -1), (1, 0), (-1, 0)):
+            nx, ny = cx + dx, cy + dy
 
             if 0 <= nx < N and 0 <= ny < M and temp[nx][ny] == 0:
                 temp[nx][ny] = 2
                 q.append((nx, ny))
 
-    cnt = 0
-
+    current_safe = 0
     for i in range(N):
-        cnt += temp[i].count(0)
+        current_safe += temp[i].count(0)
+    safe = max(safe, current_safe)
 
-    return cnt
+def dfs(n, lst):
+    if n == 3:
+        temp = copy.deepcopy(arr)
 
+        for x, y in lst:
+            temp[x][y] = 1
+
+        spread_virus(temp)
+
+        return
+    for i in range(N):
+        for j in range(M):
+            if arr[i][j] == 0 and not v[i][j]:
+                v[i][j] = True
+                dfs(n + 1, lst + [[i, j]])
+                v[i][j] = False
 
 N, M = map(int, input().split())
-lab = [[0] *  M for _ in range(N)]
-
-wall = []
+arr = [list(map(int, input().split())) for _ in range(N)]
+v = [[False] * M for _ in range(N)]
 virus = []
-ans = 0
-
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
-
-for i in range(N):
-    lab[i] = list(map(int, input().split()))
+safe = 0
 
 for i in range(N):
     for j in range(M):
-        if lab[i][j] == 0:
-            wall.append([i, j])
-        elif lab[i][j] == 2:
+        if arr[i][j] == 2:
             virus.append([i, j])
+dfs(0, [])
 
-wall_list = list(combinations(wall, 3))
-
-for w in wall_list:
-    temp = copy.deepcopy(lab)
-    temp[w[0][0]][w[0][1]] = 1
-    temp[w[1][0]][w[1][1]] = 1
-    temp[w[2][0]][w[2][1]] = 1
-
-    ans = max(ans, bfs())
-
-print(ans)
+print(safe)
