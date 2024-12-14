d59d16a2d1ef0b1779b0853784ced43e6646093b
yeji0214 <1225yj50@naver.com>
2024-12-14 12:21:56 +0900

    프로그래머스 - 없는 숫자 더하기

--- Programmers/다음 큰 숫자.py
+++ Programmers/다음 큰 숫자.py
@@ -1,11 +1,13 @@
 def solution(n):
     binary = str(bin(n)[2:])
-    cnt = binary.count('1')
-    ans = n + 1
+    cnt = binary.count(1)
+    ans = n
     
     while True:
         b = str(bin(ans)[2:])
-        if cnt == b.count('1'):
+        if cnt == b.count(1):
             return ans
         else:
             ans += 1
+
+print(solution(78))
