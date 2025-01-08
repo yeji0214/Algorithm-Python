bf0aecc8b5db28dfd30bacbe056a8aa1df92a1d1
yeji0214 <1225yj50@naver.com>
2025-01-08 14:01:15 +0900

    백준 1076

---
+++ Baekjoon/1076.py
@@ -0,0 +1,6 @@
+resistance = {'black': [0, 1], 'brown': [1, 10], 'red': [2, 100], 'orange': [3, 1000], 'yellow': [4, 10000], 'green': [5, 100000], 'blue': [6, 1000000], 'violet': [7, 10000000], 'grey': [8, 100000000], 'white': [9, 1000000000]}
+colors = list(input() for _ in range(3))
+
+value = int(str(resistance[colors[0]][0]) + str(resistance[colors[1]][0]))
+
+print(value * resistance[colors[2]][1])
