fdf1669074275bb40a015d41677fff5eb23685c7
yeji0214 <1225yj50@naver.com>
2025-07-09 23:05:01 +0900

    백준 11399 재풀이

--- Baekjoon/11399.py
+++ Baekjoon/11399.py
@@ -7,3 +7,15 @@ for i in range(1, N+1):
     ans += sum(P[:i])
 
 print(ans)
+
+###
+
+N = int(input())
+ans = 0
+
+P = sorted(list(map(int, input().split())))
+
+for i in range(N):
+    ans += sum(P[:i + 1])
+
+print(ans)
