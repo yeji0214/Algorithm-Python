5ff4014547fe3909000964fde27713e7a18f05b7
yeji0214 <1225yj50@naver.com>
2025-07-11 13:00:36 +0900

    백준 1141 재풀이

--- Baekjoon/1141.py
+++ Baekjoon/1141.py
@@ -1,42 +1,60 @@
-# 그리디
-# 내 풀이
+# # 그리디
+# # 내 풀이
+# N = int(input())
+# words = []
+# ans = 0
+
+# for _ in range(N):
+#     words.append(input())
+
+# for i in range(N):
+#     for j in range(N):
+#         if i == j:
+#             continue
+#         if words[j].find(words[i]) == 0: # 0일 때 접두사
+#             words[i] = ""
+        
+# for j in words:
+#     if j != "":
+#         ans += 1
+
+# print(ans)
+
+# # 정렬 사용
+# N = int(input())
+# words = []
+# ans = 0
+
+# for _ in range(N):
+#     words.append(input())
+
+# words.sort(key=len)
+
+# for i in range(N):
+#     for j in range(i+1, N):
+#         if words[j].find(words[i]) == 0: # 0일 때 접두사
+#             words[i] = ""
+        
+# for j in words:
+#     if j != "":
+#         ans += 1
+
+# print(ans)
+
+
+###
+
 N = int(input())
-words = []
-ans = 0
+sentences = []
 
 for _ in range(N):
-    words.append(input())
+    sentences.append(input())
 
 for i in range(N):
     for j in range(N):
         if i == j:
             continue
-        if words[j].find(words[i]) == 0: # 0일 때 접두사
-            words[i] = ""
-        
-for j in words:
-    if j != "":
-        ans += 1
+        if sentences[j].find(sentences[i]) == 0:
+            sentences[i] = ""
 
-print(ans)
-
-# 정렬 사용
-N = int(input())
-words = []
-ans = 0
-
-for _ in range(N):
-    words.append(input())
-
-words.sort(key=len)
-
-for i in range(N):
-    for j in range(i+1, N):
-        if words[j].find(words[i]) == 0: # 0일 때 접두사
-            words[i] = ""
-        
-for j in words:
-    if j != "":
-        ans += 1
-
-print(ans)
+print(N - sentences.count(""))
