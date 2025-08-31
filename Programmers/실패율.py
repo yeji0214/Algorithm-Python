1c8498c611ab74c6b19753751768a262ce4631a5
yeji0214 <1225yj50@naver.com>
2025-08-31 11:58:51 +0900

    프로그래머스 실패율 재풀이

--- Programmers/실패율.py
+++ Programmers/실패율.py
@@ -1,15 +1,30 @@
-def solution(N, stages):
-    answer = {}
-    player = len(stages)
+# def solution(N, stages):
+#     answer = {}
+#     player = len(stages)
 
+#     for i in range(1, N + 1):
+#         if player != 0:
+#             fail = stages.count(i)
+#             answer[i] = fail / player
+#             player -= fail
+#         else:
+#             answer[i] = 0
+#     return sorted(answer, key=lambda x: answer[x], reverse=True)
+
+def solution(N, stages):
+    result = {}
+    total = len(stages)
+    
     for i in range(1, N + 1):
-        if player != 0:
-            fail = stages.count(i)
-            answer[i] = fail / player
-            player -= fail
+        count = stages.count(i)
+        
+        if total == 0:
+            result[i] = 0
         else:
-            answer[i] = 0
-    return sorted(answer, key=lambda x: answer[x], reverse=True)
+            result[i] = count / total
+            
+        total -= count
+        
+    return [k for k, v in sorted(result.items(), key=lambda item: (-item[1], item[0]))]
 
 print(solution(5, [2, 1, 2, 6, 2, 4, 3, 3]))
-# print(solution(4, [4,4,4,4,4]))
