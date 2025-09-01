817023a1fbbf7a479034969bb029eca91475bb28
yeji0214 <1225yj50@naver.com>
2025-09-01 13:19:25 +0900

    프로그래머스 짝지어 제거하기 재풀이

--- Programmers/짝지어 제거하기.py
+++ Programmers/짝지어 제거하기.py
@@ -1,17 +1,31 @@
+# def solution(s):
+#     stack = []
+
+#     for i in s:
+#         if len(stack) == 0:
+#             stack.append(i)
+#         elif stack[-1] == i:
+#             stack.pop()
+#         else:
+#             stack.append(i)
+
+#     if len(stack) == 0:
+#         return 1
+#     else:
+#         return 0
+
+# print(solution('cdcd'))
+
+
 def solution(s):
     stack = []
-
+    
     for i in s:
-        if len(stack) == 0:
-            stack.append(i)
-        elif stack[-1] == i:
+        if len(stack) > 0 and stack[-1] == i:
             stack.pop()
         else:
             stack.append(i)
-
-    if len(stack) == 0:
-        return 1
-    else:
+            
+    if len(stack) > 0:
         return 0
-
-print(solution('cdcd'))
+    return 1
