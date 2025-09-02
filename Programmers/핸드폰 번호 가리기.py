1e380fab40eb9c2f2d217312dcb05ed7e9fb5582
yeji0214 <1225yj50@naver.com>
2025-09-02 10:08:07 +0900

    프로그래머스 핸드폰 번호 가리기 재풀이

--- Programmers/핸드폰 번호 가리기.py
+++ Programmers/핸드폰 번호 가리기.py
@@ -1,6 +1,15 @@
+# def solution(phone_number):
+#     ans = ''
+#     for n in range(len(phone_number) - 4):
+#         ans += '*'
+#     ans += phone_number[-4:]
+#     return ans
+
+###
+
 def solution(phone_number):
     ans = ''
-    for n in range(len(phone_number) - 4):
-        ans += '*'
+    ans += ('*' * (len(phone_number) - 4))
     ans += phone_number[-4:]
+    
     return ans
