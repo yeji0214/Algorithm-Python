701e6f1fa94a0ada56fac7bb03a283557e46f071
yeji0214 <1225yj50@naver.com>
2025-09-12 14:38:19 +0900

    백준 2003 재풀이

--- Baekjoon/2003.py
+++ Baekjoon/2003.py
@@ -1,20 +1,37 @@
-# 브루트포스, 투포인터
+# # 브루트포스, 투포인터
+# N, M = map(int, input().split())
+# num = list(map(int, input().split()))
+# s = e = 0
+# ans = 0
+
+# while s <= e < N:
+#     if M == sum(num[s:e+1]):
+#         ans += 1
+#         s += 1
+#         if s > e:
+#             e += 1
+#     elif M > sum(num[s:e+1]):
+#         e += 1
+#     else:
+#         s += 1
+#         if s > e:
+#             e += 1
+
+# print(ans)
+
 N, M = map(int, input().split())
-num = list(map(int, input().split()))
+nums = list(map(int, input().split()))
+
 s = e = 0
 ans = 0
 
-while s <= e < N:
-    if M == sum(num[s:e+1]):
+while e < N:
+    if sum(nums[s:e+1]) == M:
         ans += 1
         s += 1
-        if s > e:
-            e += 1
-    elif M > sum(num[s:e+1]):
+    elif sum(nums[s:e+1]) < M:
         e += 1
     else:
         s += 1
-        if s > e:
-            e += 1
 
 print(ans)
