bfff67824e106d24cfc258407333c47d1576d1c8
yeji0214 <1225yj50@naver.com>
2025-10-25 11:32:04 +0900

    백준 17086

--- Baekjoon/17086.py
+++ Baekjoon/17086.py
@@ -1,21 +1,70 @@
+# from collections import deque
+
+# def bfs():
+#     q = deque()
+#     for i, j in shark:
+#         q.append((i, j))
+#         visited[i][j] = True
+
+#     while q:
+#         cx, cy = q.popleft()
+
+#         for i in range(8):
+#             nx, ny = cx + dx[i], cy + dy[i]
+            
+#             if 0 <= nx < N and 0 <= ny < M and not visited[nx][ny]:
+#                 arr[nx][ny] = arr[cx][cy] + 1
+#                 q.append((nx, ny))
+#                 visited[nx][ny] = True
+
+
+# N, M = map(int, input().split())
+# arr = [[0] * M for _ in range(N)]
+# visited = [[False] * M for _ in range(N)]
+# shark = []
+# ans = 0
+
+# dx = [-1, 1, 0, 0, -1, -1, 1, 1]
+# dy = [0, 0, -1, 1, 1, -1, 1, -1]
+
+# for i in range(N):
+#     arr[i] = list(map(int, input().split()))
+
+# for i in range(N):
+#     for j in range(M):
+#         if arr[i][j] == 1:
+#             shark.append([i, j])
+
+# bfs()
+
+# for a in arr:
+#     ans = max(ans, max(a))
+
+# print(ans - 1)
+
+
 from collections import deque
 
 def bfs():
     q = deque()
-    for i, j in shark:
-        q.append((i, j))
-        visited[i][j] = True
+    for s in shark:
+        sx, sy = s[0], s[1]
+        q.append((sx, sy))
+        visited[sx][sy] = True
 
     while q:
         cx, cy = q.popleft()
 
-        for i in range(8):
-            nx, ny = cx + dx[i], cy + dy[i]
-            
+        for k in range(8):
+            nx, ny = cx + dx[k], cy + dy[k]
+
             if 0 <= nx < N and 0 <= ny < M and not visited[nx][ny]:
-                arr[nx][ny] = arr[cx][cy] + 1
-                q.append((nx, ny))
+                if arr[nx][ny] == 0:
+                    arr[nx][ny] = arr[cx][cy] + 1
+                else:
+                    arr[nx][ny] = min(arr[nx][ny], arr[cx][cy] + 1)
                 visited[nx][ny] = True
+                q.append((nx, ny))
 
 
 N, M = map(int, input().split())
@@ -37,7 +86,7 @@ for i in range(N):
 
 bfs()
 
-for a in arr:
-    ans = max(ans, max(a))
+for i in range(N):
+    ans = max(ans, max(arr[i]))
 
 print(ans - 1)
