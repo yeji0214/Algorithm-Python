f2803bf9686dec4d767334fc6a1cbd683d4ed5a2
yeji0214 <1225yj50@naver.com>
2025-10-26 13:11:59 +0900

    백준 21736

--- Baekjoon/21736.py
+++ Baekjoon/21736.py
@@ -1,43 +1,90 @@
+# from collections import deque
+
+# def start(x, y):
+#     q = deque()
+#     q.append((x, y))
+#     visited[x][y] = True
+#     friend = 0
+
+#     while q:
+#         cx, cy = q.popleft()
+
+#         for i in range(4):
+#             nx, ny = cx + dx[i], cy + dy[i]
+#             if 0 <= nx < N and 0 <= ny < M:
+#                 if campas[nx][ny] == 'O' and not visited[nx][ny]:
+#                     visited[nx][ny] = True
+#                     q.append((nx, ny))
+#                 elif campas[nx][ny] == 'P' and not visited[nx][ny]:
+#                     visited[nx][ny] = True
+#                     q.append((nx, ny))
+#                     friend += 1
+
+#     return friend
+
+# N, M = map(int, input().split())
+# campas = [[] * M for _ in range(N)]
+# visited = [[False] * M for _ in range(N)]
+
+# dx = [-1, 1, 0, 0]
+# dy = [0, 0, -1, 1]
+
+# for i in range(N):
+#     campas[i] = list(input())
+
+# for i in range(N):
+#     for j in range(M):
+#         if campas[i][j] == 'I':
+#             ans = start(i, j)
+#             if ans == 0:
+#                 print('TT')
+#             else:
+#                 print(ans)
+#             exit(0)
+
+
 from collections import deque
 
-def start(x, y):
+def bfs(sx, sy):
+    ans = 0
     q = deque()
-    q.append((x, y))
-    visited[x][y] = True
-    friend = 0
+    q.append((sx, sy))
+    visited[sx][sy] = True
 
     while q:
         cx, cy = q.popleft()
 
-        for i in range(4):
-            nx, ny = cx + dx[i], cy + dy[i]
-            if 0 <= nx < N and 0 <= ny < M:
-                if campas[nx][ny] == 'O' and not visited[nx][ny]:
-                    visited[nx][ny] = True
-                    q.append((nx, ny))
-                elif campas[nx][ny] == 'P' and not visited[nx][ny]:
-                    visited[nx][ny] = True
-                    q.append((nx, ny))
-                    friend += 1
+        for k in range(4):
+            nx, ny = cx + dx[k], cy + dy[k]
 
-    return friend
+            if 0 <= nx < N and 0 <= ny < M and not visited[nx][ny] and arr[nx][ny] != 'X':
+                if arr[nx][ny] == 'P':
+                    ans += 1
+                visited[nx][ny] = True
+                q.append((nx, ny))
+
+    return ans
 
 N, M = map(int, input().split())
-campas = [[] * M for _ in range(N)]
+arr = [[''] * M for _ in range(N)]
 visited = [[False] * M for _ in range(N)]
+
+sx = sy = 0
 
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
 for i in range(N):
-    campas[i] = list(input())
+    arr[i] = list(input())
 
 for i in range(N):
-    for j in range(M):
-        if campas[i][j] == 'I':
-            ans = start(i, j)
-            if ans == 0:
-                print('TT')
-            else:
-                print(ans)
-            exit(0)
+    if 'I' in arr[i]:
+        sx = i
+        sy = arr[i].index('I')
+        break
+
+res = bfs(sx, sy)
+if res == 0:
+    print('TT')
+else:
+    print(res)
