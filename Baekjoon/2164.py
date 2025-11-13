31f2d12c943a460d338fcfbea3782c12a49fd3da
yeji0214 <1225yj50@naver.com>
2025-11-13 11:30:57 +0900

    백준 2164 재풀이

--- Baekjoon/2164.py
+++ Baekjoon/2164.py
@@ -1,14 +1,29 @@
-# 자료구조
+# # 자료구조
+# from collections import deque
+
+# N = int(input())
+# q = deque()
+
+# for i in range(1, N+1):
+#     q.append(i)
+
+# while len(q) > 1:
+#     q.popleft()
+#     q.append(q.popleft())
+
+# print(q.pop())
+
 from collections import deque
 
 N = int(input())
 q = deque()
 
-for i in range(1, N+1):
+for i in range(1, N + 1):
     q.append(i)
 
-while len(q) > 1:
+while True:
+    if len(q) == 1:
+        print(q.popleft())
+        exit(0)
     q.popleft()
     q.append(q.popleft())
-
-print(q.pop())
