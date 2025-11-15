93dc7098d566156d431d6929875992e9ea9f059d
yeji0214 <1225yj50@naver.com>
2025-11-15 12:23:19 +0900

    백준 1764 재풀이

--- Baekjoon/1764.py
+++ Baekjoon/1764.py
@@ -1,7 +1,24 @@
-N, M = map(int, input().split())
+# N, M = map(int, input().split())
 
+# unheard = []
+# unseen = []
+
+# for _ in range(N):
+#     unheard.append(input())
+
+# for _ in range(M):
+#     unseen.append(input())
+
+# people = sorted(list(set(unheard) & set(unseen)))
+
+# print(len(people))
+# print(*people, sep='\n')
+
+
+N, M = map(int, input().split())
 unheard = []
 unseen = []
+ans = []
 
 for _ in range(N):
     unheard.append(input())
@@ -9,7 +26,7 @@ for _ in range(N):
 for _ in range(M):
     unseen.append(input())
 
-people = sorted(list(set(unheard) & set(unseen)))
-
-print(len(people))
-print(*people, sep='\n')
+# &는 집합 간의 교집합을 구하는 연산자이기 때문에 set으로 형변환을 해줘야 함
+ans = sorted(set(unheard) & set(unseen))
+print(len(ans))
+print(*ans, sep='\n')
