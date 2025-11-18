ff028a3ffbd9010c5ffa23169dd46331a7edb894
yeji0214 <1225yj50@naver.com>
2025-11-18 14:32:20 +0900

    백준 3273 재풀이

--- Baekjoon/3273.py
+++ Baekjoon/3273.py
@@ -1,5 +1,26 @@
+# n = int(input())
+# nums = sorted(list(map(int, input().split())))
+# x = int(input())
+
+# s = 0
+# e = n - 1
+# ans = 0
+
+# while s < e:
+#     res = nums[s] + nums[e]
+#     if res == x:
+#         s += 1
+#         e -= 1
+#         ans += 1
+#     elif res < x:
+#         s += 1
+#     else:
+#         e -= 1
+
+# print(ans)
+
 n = int(input())
-nums = sorted(list(map(int, input().split())))
+a = sorted(list(map(int, input().split())))
 x = int(input())
 
 s = 0
@@ -7,14 +28,14 @@ e = n - 1
 ans = 0
 
 while s < e:
-    res = nums[s] + nums[e]
-    if res == x:
+    result = a[s] + a[e]
+    if result < x:
+        s += 1
+    elif result > x:
+        e -= 1
+    else:
         s += 1
         e -= 1
         ans += 1
-    elif res < x:
-        s += 1
-    else:
-        e -= 1
 
 print(ans)
