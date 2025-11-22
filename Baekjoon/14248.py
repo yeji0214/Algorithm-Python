da9b442994aad51bd253313228643870af5a1ba5
yeji0214 <1225yj50@naver.com>
2025-11-22 13:47:24 +0900

    백준 14248 재풀이

--- Baekjoon/14248.py
+++ Baekjoon/14248.py
@@ -1,15 +1,40 @@
-# 그래프, dfs
-def dfs(s):
-    if s > 0 and s <= n and not visited[s]:
-        visited[s] = True
-        dfs(s-stones[s-1])
-        dfs(s+stones[s-1])
+# # 그래프, dfs
+# def dfs(s):
+#     if s > 0 and s <= n and not visited[s]:
+#         visited[s] = True
+#         dfs(s-stones[s-1])
+#         dfs(s+stones[s-1])
+
+# n = int(input())
+# stones = list(map(int, input().split()))
+# s = int(input())
+# visited = [False] * (n+1)
+
+# dfs(s)
+
+# print(visited.count(True))
+
+from collections import deque
+
+def bfs(s):
+    q = deque()
+    q.append(s)
+    visited[s] = True
+
+    while q:
+        c = q.popleft()
+        move = [c - A[c], c + A[c]]
+
+        for m in move:
+            if 0 <= m < n and not visited[m]:
+                visited[m] = True
+                q.append(m)
 
 n = int(input())
-stones = list(map(int, input().split()))
+A = list(map(int, input().split()))
 s = int(input())
-visited = [False] * (n+1)
+visited = [False] * n
 
-dfs(s)
+bfs(s - 1)
 
 print(visited.count(True))
