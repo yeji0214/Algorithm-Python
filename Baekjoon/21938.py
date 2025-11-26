5f7584640fcb387124dfcf2c2505cc506df55b6a
yeji0214 <1225yj50@naver.com>
2025-11-26 17:58:54 +0900

    백준 21938 재풀이

--- Baekjoon/21938.py
+++ Baekjoon/21938.py
@@ -1,3 +1,51 @@
+# from collections import deque
+
+# def bfs(i, j):
+#     q = deque()
+#     q.append((i, j))
+#     visited[i][j] = True
+
+#     while q:
+#         cx, cy = q.popleft()
+
+#         for k in range(4):
+#             nx, ny = cx + dx[k], cy + dy[k]
+
+#             if 0 <= nx < N and 0 <= ny < M and not visited[nx][ny] and pixel[nx][ny] == 255:
+#                 visited[nx][ny] = True
+#                 q.append((nx, ny))
+
+# N, M = map(int, input().split())
+# pixel = [[] * M for _ in range(N)]
+# visited = [[False] * M for _ in range(N)]
+# ans = 0
+
+# for i in range(N):
+#     rgb = list(map(int, input().split()))
+#     for j in range(M):
+#         p = sum(rgb[j * 3:(j + 1) * 3]) / 3
+#         pixel[i].append(p)
+
+# T = int(input())
+
+# dx = [-1, 1, 0, 0]
+# dy = [0, 0, -1, 1]
+
+# for i in range(N):
+#      for j in range(M):
+#         if pixel[i][j] >= T:
+#             pixel[i][j] = 255
+#         else:
+#             pixel[i][j] = 0
+
+# for i in range(N):
+#      for j in range(M):
+#         if not visited[i][j] and pixel[i][j] == 255:
+#              bfs(i, j)
+#              ans += 1
+
+# print(ans)
+
 from collections import deque
 
 def bfs(i, j):
@@ -16,15 +64,13 @@ def bfs(i, j):
                 q.append((nx, ny))
 
 N, M = map(int, input().split())
-pixel = [[] * M for _ in range(N)]
+rgb = [[0] * M * 3 for _ in range(N)]
+pixel = [[0] * M for _ in range(N)]
 visited = [[False] * M for _ in range(N)]
 ans = 0
 
 for i in range(N):
-    rgb = list(map(int, input().split()))
-    for j in range(M):
-        p = sum(rgb[j * 3:(j + 1) * 3]) / 3
-        pixel[i].append(p)
+    rgb[i] = list(map(int, input().split()))
 
 T = int(input())
 
@@ -32,16 +78,17 @@ dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
 for i in range(N):
-     for j in range(M):
-        if pixel[i][j] >= T:
+    for j in range(M):
+        result = sum(rgb[i][j*3:j*3+3]) / 3
+        if result >= T:
             pixel[i][j] = 255
         else:
             pixel[i][j] = 0
 
 for i in range(N):
-     for j in range(M):
-        if not visited[i][j] and pixel[i][j] == 255:
-             bfs(i, j)
-             ans += 1
+    for j in range(M):
+        if pixel[i][j] == 255 and not visited[i][j]:
+            bfs(i, j)
+            ans += 1
 
 print(ans)
