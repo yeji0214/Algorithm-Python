4c3772d4fb8a990235566a802387384643a40859
yeji0214 <1225yj50@naver.com>
2026-02-04 13:02:06 +0900

    백준 13335 재풀이

--- Baekjoon/13335.py
+++ Baekjoon/13335.py
@@ -1,17 +1,34 @@
-# 큐
-n, w, L = map(int, input().split())
-a = list(map(int, input().split()))
+# # 큐
+# n, w, L = map(int, input().split())
+# a = list(map(int, input().split()))
 
-q = [0] * w
+# q = [0] * w
+# time = 0
+
+# while q:
+#     time += 1
+#     q.pop(0)
+#     if a:
+#         if sum(q) + a[0] <= L:
+#             q.append(a.pop(0))
+#         else:
+#             q.append(0)
+
+# print(time)
+
+n, w, L = map(int, input().split())
+A = list(map(int, input().split()))
+bridge = [0] * w
 time = 0
 
-while q:
+while A:
+    bridge = bridge[1:] + [0]
+    if (sum(bridge) + A[0]) <= L and bridge[-1] == 0:
+        bridge[-1] = A.pop(0)
     time += 1
-    q.pop(0)
-    if a:
-        if sum(q) + a[0] <= L:
-            q.append(a.pop(0))
-        else:
-            q.append(0)
+
+while bridge.count(0) < w:
+    bridge = bridge[1:] + [0]
+    time += 1
 
 print(time)
