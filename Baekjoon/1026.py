ee8dfeb54714490c2d6bc466f3fcc6ed3da01aac
yeji0214 <1225yj50@naver.com>
2026-02-05 11:24:48 +0900

    백준 1026 재풀이

--- Baekjoon/1026.py
+++ Baekjoon/1026.py
@@ -1,11 +1,21 @@
+# N = int(input())
+# A = sorted(list(map(int, input().split())))
+# B = list(map(int, input().split()))
+
+# ans = 0
+
+# for i in range(N):
+#     ans += A[i] * max(B)
+#     B[B.index(max(B))] = -1
+
+# print(ans)
+
 N = int(input())
 A = sorted(list(map(int, input().split())))
-B = list(map(int, input().split()))
-
+B = sorted(list(map(int, input().split())), reverse=True)
 ans = 0
 
 for i in range(N):
-    ans += A[i] * max(B)
-    B[B.index(max(B))] = -1
+    ans += A[i] * B[i]
 
 print(ans)
