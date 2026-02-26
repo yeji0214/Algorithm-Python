a958cf01588c10bba40281674bc263e29ad2d9cc
YEJI <1225yj50@naver.com>
2026-02-26 11:41:00 +0900

    백준 1436

--- Baekjoon/1436.py
+++ Baekjoon/1436.py
@@ -1,7 +1,20 @@
+# N = int(input())
+
+# num = 666
+# cnt = 0
+
+# while True:
+#     if '666' in str(num):
+#         cnt += 1
+#     if cnt == N:
+#         print(num)
+#         exit(0)
+#     num += 1
+
 N = int(input())
 
-num = 666
 cnt = 0
+num = 666
 
 while True:
     if '666' in str(num):
