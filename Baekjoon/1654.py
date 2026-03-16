7fdaa9e4a13fc80096a7ca07bd89f5cd6ab7df88
YEJI <1225yj50@naver.com>
2026-03-16 14:24:41 +0900

    백준 1654

--- Baekjoon/1654.py
+++ Baekjoon/1654.py
@@ -1,21 +1,42 @@
-# 이진 탐색
-def cutter():
+# # 이진 탐색
+# def cutter():
+#     s, e = 1, max(wires)
+
+#     while s <= e:
+#         total = 0 # 만들 수 있는 랜선의 개수
+#         mid = (s+e) // 2
+#         for i in wires:
+#             total += i // mid
+
+#         if total < N: # 더 많이 잘라야 함
+#             e = mid - 1
+#         else:
+#             s = mid + 1
+
+#     return e
+
+# K, N = map(int, input().split())
+# wires = list(int(input()) for _ in range(K))
+
+# print(cutter())
+
+def cutting():
     s, e = 1, max(wires)
 
     while s <= e:
-        total = 0 # 만들 수 있는 랜선의 개수
-        mid = (s+e) // 2
-        for i in wires:
-            total += i // mid
+        mid = (s + e) // 2
+        total = 0
+        for w in wires:
+            total += w // mid
 
-        if total < N: # 더 많이 잘라야 함
+        if total < M:
             e = mid - 1
         else:
             s = mid + 1
 
     return e
 
-K, N = map(int, input().split())
-wires = list(int(input()) for _ in range(K))
+N, M = map(int, input().split())
+wires = [int(input()) for _ in range(N)]
 
-print(cutter())
+print(cutting())
