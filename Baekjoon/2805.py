195418baa7c7f02502d51a2793198b398d385b92
YEJI <1225yj50@naver.com>
2026-03-17 17:44:52 +0900

    백준 2805 재풀이

--- Baekjoon/2805.py
+++ Baekjoon/2805.py
@@ -1,25 +1,48 @@
-# 이진 탐색
-def cutter():
-    s = 0
-    e = max(trees)
-    height = 0
+# # 이진 탐색
+# def cutter():
+#     s = 0
+#     e = max(trees)
+#     height = 0
+
+#     while s <= e:
+#         total = 0
+#         mid = (s + e) // 2
+#         for i in trees:
+#             if i > mid:
+#                 total += i - mid
+
+#         if total >= M:
+#             s = mid + 1
+#             height = mid
+#         else:
+#             e = mid - 1
+
+#     return height
+
+# N, M = map(int, input().split())
+# trees = list(map(int, input().split()))
+
+# print(cutter())
+
+def cutting():
+    s, e = 1, max(trees)
 
     while s <= e:
-        total = 0
         mid = (s + e) // 2
-        for i in trees:
-            if i > mid:
-                total += i - mid
+        total = 0
 
-        if total >= M:
-            s = mid + 1
-            height = mid
-        else:
+        for t in trees:
+            if t > mid:
+                total += (t - mid)
+
+        if total < M:
             e = mid - 1
+        else:
+            s = mid + 1
 
-    return height
+    return e
 
 N, M = map(int, input().split())
 trees = list(map(int, input().split()))
 
-print(cutter())
+print(cutting())
