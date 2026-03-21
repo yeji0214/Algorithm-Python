b00ff4c28b67cef472f8c8366b15b1111e2fcdb4
YEJI <1225yj50@naver.com>
2026-03-21 16:40:52 +0900

    백준 4949 재풀이

--- Baekjoon/4949.py
+++ Baekjoon/4949.py
@@ -1,24 +1,52 @@
-while True:
-    str = input()
-    stack = []
-    no = 0
+# while True:
+#     str = input()
+#     stack = []
+#     no = 0
 
-    if str == '.':
+#     if str == '.':
+#         exit(0)
+#     for s in str:
+#         if s == '[' or s == '(':
+#             stack.append(s)
+#         elif s == ']':
+#             if len(stack) == 0 or stack.pop() != '[':
+#                 no = 1
+#                 break
+#         elif s == ')':
+#             if len(stack) == 0 or stack.pop() != '(':
+#                 no = 1
+#                 break
+#     if len(stack) > 0:
+#         no = 1
+#     if no == 1:
+#         print("no")
+#     else:
+#         print("yes")
+
+while True:
+    S = input()
+    if S == '.':
         exit(0)
-    for s in str:
-        if s == '[' or s == '(':
+    stack = []
+    ans = True
+
+    for s in S:
+        if s == '(' or s == '[':
             stack.append(s)
-        elif s == ']':
-            if len(stack) == 0 or stack.pop() != '[':
-                no = 1
-                break
         elif s == ')':
-            if len(stack) == 0 or stack.pop() != '(':
-                no = 1
+            if stack and stack[-1] == '(':
+                stack.pop(-1)
+            else:
+                ans = False
                 break
-    if len(stack) > 0:
-        no = 1
-    if no == 1:
-        print("no")
-    else:
+        elif s == ']':
+            if stack and stack[-1] == '[':
+                stack.pop(-1)
+            else:
+                ans = False
+                break
+
+    if len(stack) == 0 and ans:
         print("yes")
+    else:
+        print("no")
