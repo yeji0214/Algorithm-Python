d4bb43afe40dc280998c2385a8fec8b394e2498e
YEJI <1225yj50@naver.com>
2026-04-28 15:41:57 +0900

    리트코드 BinarySearch

---
+++ Leetcode/BinarySearch.py
@@ -0,0 +1,15 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        s, e = 0, len(nums) - 1
+
+        while s <= e:
+            mid = (s + e) // 2
+
+            if nums[mid] == target:
+                return mid
+            if nums[mid] > target:
+                e = mid - 1
+            else:
+                s = mid + 1
+
+        return -1
