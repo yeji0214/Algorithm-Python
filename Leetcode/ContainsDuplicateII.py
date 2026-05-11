3ecaacdbc512ebc8bf0d152c3b04bff33874f879
YEJI <1225yj50@naver.com>
2026-05-11 11:16:04 +0900

    리트코드 ContainsDuplicateII

---
+++ Leetcode/ContainsDuplicateII.py
@@ -0,0 +1,14 @@
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        index = {}
+
+        for i in range(len(nums)):
+            if nums[i] in index:
+                if (i - index[nums[i]]) <= k:
+                    return True
+                else:
+                    index[nums[i]] = i
+            else:
+                index[nums[i]] = i
+
+        return False
