9a7b4f0339bf0c50f582666e9e384fc087193f29
YEJI <1225yj50@naver.com>
2026-06-20 22:16:03 +0900

    리트코드 ThirdMaximumNumber

---
+++ Leetcode/ThirdMaximumNumber.py
@@ -0,0 +1,8 @@
+class Solution:
+    def thirdMax(self, nums: List[int]) -> int:
+        nums = sorted(list(set(nums)), reverse=True)
+
+        if len(nums) < 3:
+            return nums[0]
+        else:
+            return nums[2]
