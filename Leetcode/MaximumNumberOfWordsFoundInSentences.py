05ceb4e92ba1c054e6e33f947e1416742279dda1
YEJI <1225yj50@naver.com>
2026-09-07 21:00:00 +0900

    리트코드 MaximumNumberOfWordsFoundInSentences

---
+++ Leetcode/MaximumNumberOfWordsFoundInSentences.py
@@ -0,0 +1,8 @@
+class Solution:
+    def mostWordsFound(self, sentences: List[str]) -> int:
+        ans = 0
+
+        for s in sentences:
+            ans = max(ans, len(s.split()))
+
+        return ans
