381d83ede30e7e12fe17179463820cd4790a19ce
YEJI <1225yj50@naver.com>
2026-09-08 23:55:40 +0900

    리트코드 FindNumbersWithEvenNumberOfDigits

---
+++ Leetcode/FindNumbersWithEvenNumberOfDigits.py
@@ -0,0 +1,9 @@
+class Solution:
+    def findNumbers(self, nums: List[int]) -> int:
+        ans = 0
+
+        for n in nums:
+            if len(str(n)) % 2 == 0:
+                ans += 1
+
+        return ans
