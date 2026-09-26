98d1281ac889e894612e5d19d4a5fd1bffdb0e90
YEJI <1225yj50@naver.com>
2026-09-26 10:20:41 +0900

    리트코드 CountOddNumbersInAnIntervalRange

---
+++ Leetcode/CountOddNumbersInAnIntervalRange.py
@@ -0,0 +1,8 @@
+class Solution:
+    def countOdds(self, low: int, high: int) -> int:
+        ans = (high - low) // 2
+
+        if low % 2 == 1 or high % 2 == 1:
+            ans +=1 
+
+        return ans
